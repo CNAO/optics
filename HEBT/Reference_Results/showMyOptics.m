ce3e08b5d66d2bebc5c827bf488321b0a2fdcac3
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-12-14 21:46:27 +0100

    comparing HEBT optics

--- HEBT/Reference_Results/showMyOptics.m
+++ HEBT/Reference_Results/showMyOptics.m
@@ -5,24 +5,55 @@
 pathToLibrary="..\..\..\MatLabTools";
 addpath(genpath(pathToLibrary));
 
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% show single optics files
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %% files
-% % original optics
-% opticsFileName="..\lineu_example_optics.tfs";
-% geometryFileName="..\lineu_example_geometry.tfs";
-% rMatrixFileName="..\lineu_example_rmatrix.tfs";
-% optics of short line
-opticsFileName="..\example_short_optics.tfs";
-geometryFileName="..\example_short_geometry.tfs";
-rMatrixFileName="..\example_short_rmatrix.tfs";
+opticsFileName="../linez_lgen_optics.tfs";
+geometryFileName="../linez_lgen_geometry.tfs";
+rMatrixFileName="../linez_lgen_rmatrix.tfs";
 
 %% acquire data
 optics = ParseTfsTable(opticsFileName,'optics');
 [Qx,Qy,Chrx,Chry,Laccel,headerNames,headerValues] = ...
     ParseTfsTableHeader(opticsFileName);
-geometry = ParseTfsTable(geometryFileName,'geometry');
+geometries = ParseTfsTable(geometryFileName,'geometry');
 rMatrix = ParseTfsTable(rMatrixFileName,'rMatrix');
 
 %% show the optics
-myTitle="Line U (sala 2H) - Carbon - 270mm";
-ShowOptics(optics,geometry,myTitle,Laccel,Qx,Qy,Chrx,Chry);
-ShowRmatrix(rMatrix,geometry,myTitle);
+myTitle="Line Z (sala 1) - Carbon - 30mm";
+ShowOptics(optics,geometries,myTitle,Laccel,Qx,Qy,Chrx,Chry);
+ShowRmatrix(rMatrix,geometries,myTitle);
+
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% compare different optics files (in principle also different geometries)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% files
+labels=[
+    "reference"
+    "new"
+    ];
+myTitle="Line T (sala s) - Carbon - 30mm";
+opticsFileNames=[ ...
+    "linet_lgen_optics.tfs" ...
+    "../linet_lgen_optics.tfs" ...
+    ];
+geometryFileNames=[ ...
+    "linet_lgen_geometry.tfs" ...
+    "../linet_lgen_geometry.tfs" ...
+    ];
+
+%% acquire data
+optics = ParseTfsTable(opticsFileNames,'optics');
+[Qx,Qy,Chrx,Chry,Laccel,headerNames,headerValues] = ...
+    ParseTfsTableHeader(opticsFileNames);
+geometries = ParseTfsTable(geometryFileNames,'geometry');
+
+%% show the optics
+CompareOptics(optics,labels,geometries,"CO",myTitle);
+CompareOptics(optics,labels,geometries,"BET",myTitle);
+CompareOptics(optics,labels,geometries,"D",myTitle);
