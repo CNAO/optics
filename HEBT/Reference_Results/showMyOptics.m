7840ead13f313068f48898744dfa0c1f913548cb
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2023-03-02 17:07:47 +0100

    possibility to show scanned optics

--- HEBT/Reference_Results/showMyOptics.m
+++ HEBT/Reference_Results/showMyOptics.m
@@ -1,51 +1,107 @@
 % {}~
 % template analysis script
 
-%% include Matlab library
-pathToLibrary="..\..\..\MatLabTools";
-addpath(genpath(pathToLibrary));
+% %% include Matlab library
+% pathToLibrary="..\..\..\MatLabTools";
+% addpath(genpath(pathToLibrary));
+% 
+% %%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % show single optics files
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% %% files
+% opticsFileName="../xpr_iso3_lgen_optics.tfs";
+% geometryFileName="../xpr_iso3_lgen_geometry.tfs";
+% rMatrixFileName="../xpr_iso3_lgen_rmatrix.tfs";
+% 
+% %% acquire data
+% optics = ParseTfsTable(opticsFileName,'optics');
+% [Qx,Qy,Chrx,Chry,Laccel,headerNames,headerValues] = ...
+%     ParseTfsTableHeader(opticsFileName);
+% geometries = ParseTfsTable(geometryFileName,'geometry');
+% rMatrix = ParseTfsTable(rMatrixFileName,'rMatrix');
+% 
+% %% show the optics
+% myTitle="ISO3 (XPR) - Proton - 30mm";
+% ShowOptics(optics,geometries,myTitle,Laccel,Qx,Qy,Chrx,Chry);
+% ShowRmatrix(rMatrix,geometries,myTitle);
+% 
+% %%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % compare different optics files (in principle also different geometries)
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% %% files
+% labels=[
+%     "reference"
+%     "new"
+%     ];
+% myTitle="ISO3 (XPR) - Proton - 30mm";
+% opticsFileNames=[ ...
+%     "../xpr_iso3_lgen_optics.tfs" ...
+%     "../xpr_iso3_mod_lgen_optics.tfs" ...
+%     ];
+% geometryFileNames=[ ...
+%     "../xpr_iso3_lgen_geometry.tfs" ...
+%     "../xpr_iso3_mod_lgen_geometry.tfs" ...
+%     ];
+% 
+% %% acquire data
+% optics = ParseTfsTable(opticsFileNames,'optics');
+% [Qx,Qy,Chrx,Chry,Laccel,headerNames,headerValues] = ...
+%     ParseTfsTableHeader(opticsFileNames);
+% geometries = ParseTfsTable(geometryFileNames,'geometry');
+% 
+% %% show the optics
+% % CompareOptics(optics,labels,geometries,"CO",myTitle);
+% CompareOptics(optics,labels,geometries,"BET",myTitle);
+% % CompareOptics(optics,labels,geometries,"D",myTitle);
 
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% show single optics files
+% compare different optics obtained via a scan
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% files
-opticsFileName="../linez_lgen_optics.tfs";
-geometryFileName="../linez_lgen_geometry.tfs";
-rMatrixFileName="../linez_lgen_rmatrix.tfs";
+clear all;
 
-%% acquire data
-optics = ParseTfsTable(opticsFileName,'optics');
-[Qx,Qy,Chrx,Chry,Laccel,headerNames,headerValues] = ...
-    ParseTfsTableHeader(opticsFileName);
-geometries = ParseTfsTable(geometryFileName,'geometry');
-rMatrix = ParseTfsTable(rMatrixFileName,'rMatrix');
-
-%% show the optics
-myTitle="Line Z (sala 1) - Carbon - 30mm";
-ShowOptics(optics,geometries,myTitle,Laccel,Qx,Qy,Chrx,Chry);
-ShowRmatrix(rMatrix,geometries,myTitle);
-
-%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% compare different optics files (in principle also different geometries)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% files
-labels=[
-    "reference"
-    "new"
-    ];
-myTitle="Line T (sala s) - Carbon - 30mm";
+%% user settings
+whatScan="MUY";
+eleScan="X3_011B_VWN";
 opticsFileNames=[ ...
-    "linet_lgen_optics.tfs" ...
-    "../linet_lgen_optics.tfs" ...
+    "../output/xpr_iso3_lgen_optics.tfs" ...
     ];
 geometryFileNames=[ ...
-    "linet_lgen_geometry.tfs" ...
-    "../linet_lgen_geometry.tfs" ...
+    "../output/xpr_iso3_lgen_geometry.tfs" ...
     ];
+
+myTitle=sprintf("%s scan at %s - ISO3 (XPR) - Proton - 30mm",whatScan,LabelMe(eleScan));
+splitFiles=[sprintf("../output/xpr_iso3_Scan%s_%s_lgen_optics.tfs",whatScan,eleScan)];
+
+%% split files
+clear oFid;
+for ii=1:length(splitFiles)
+    fprintf("splitting file %s ...\n",splitFiles(ii));
+    iFid = fopen(splitFiles(ii),"r");
+    jj=0;
+    while ~feof(iFid)
+        tline = fgetl(iFid);
+        if (startsWith(tline,"@ NAME"))
+            jj=jj+1;
+            if (exist("oFid","var"))
+                fclose(oFid);
+            end
+            oFileName=strrep(splitFiles(ii),whatScan,sprintf("%s%03i",whatScan,jj));
+            opticsFileNames=[opticsFileNames oFileName];
+            fprintf("...saving data to file %s ...\n",oFileName);
+            oFid=fopen(oFileName,"w");
+        end
+        fprintf(oFid,"%s\n",tline);
+    end
+    fclose(oFid);
+end
+% fclose(iFid);
+fclose('all');
 
 %% acquire data
 optics = ParseTfsTable(opticsFileNames,'optics');
@@ -53,7 +109,23 @@ optics = ParseTfsTable(opticsFileNames,'optics');
     ParseTfsTableHeader(opticsFileNames);
 geometries = ParseTfsTable(geometryFileNames,'geometry');
 
+% build labels
+labels=strings(length(opticsFileNames),1);
+% - column mapping
+[ colNames, colUnits, colFacts, mapping, readFormat ] = GetColumnsAndMappingTFS('optics');
+idCol=mapping(find(strcmp(colNames,whatScan)));
+% idColX=mapping(find(strcmp(colNames,"MUX")));
+% idColY=mapping(find(strcmp(colNames,"MUY")));
+for ii=1:length(opticsFileNames)
+    idEle=find(contains(string(optics{ii,mapping(find(strcmp(colNames,"NAME")))}),eleScan));
+    if (ii==1)
+        labels(ii)=sprintf("%s=%g (ref)",whatScan,optics{ii,idCol}(idEle));
+    else
+        labels(ii)=sprintf("%s=%g",whatScan,optics{ii,idCol}(idEle));
+    end
+end
+
 %% show the optics
-CompareOptics(optics,labels,geometries,"CO",myTitle);
+% CompareOptics(optics,labels,geometries,"CO",myTitle);
 CompareOptics(optics,labels,geometries,"BET",myTitle);
-CompareOptics(optics,labels,geometries,"D",myTitle);
+% CompareOptics(optics,labels,geometries,"D",myTitle);
